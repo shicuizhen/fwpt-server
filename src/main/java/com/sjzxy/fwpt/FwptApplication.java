d83295f1679ea9c89db6096d4aab82e38078f773
shicuizhen <17692151418@163.com>
2020-11-09 20:30:22 +0800

    first commit

---
+++ src/main/java/com/sjzxy/fwpt/FwptApplication.java
@@ -0,0 +1,13 @@
+package com.sjzxy.fwpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FwptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FwptApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sjzxy/fwpt/FwptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjzxy.fwpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FwptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
